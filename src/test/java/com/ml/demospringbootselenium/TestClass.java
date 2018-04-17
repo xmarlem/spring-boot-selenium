90df825322f3af3a65c627ef895522d5ad80a7e1
xmarlem <inglembomarco@gmail.com>
2018-04-17 23:03:22 +0200

    Changed Test file according to Selenium Grid commit

--- src/test/java/com/ml/demospringbootselenium/TestClass.java
+++ src/test/java/com/ml/demospringbootselenium/TestClass.java
@@ -7,14 +7,20 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class TestClass {
+import java.net.*;
+import java.net.MalformedURLException;
+
+public class TestClass  {
 
     WebDriver driver;
 
     @Before
-    public void setUp() {
-        driver = new ChromeDriver();
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), desiredCapabilities);
     }
 
     @After
